4cc149bad31c66e1cf95530d13bca67326cbf2aa
choi wantaek <78516163+wantaekchoi@users.noreply.github.com>
2021-11-15 16:00:41 +0900

    small fix

--- exam_1/app.py
+++ exam_1/app.py
@@ -1,12 +1,12 @@
 import time
 
-def func(n):
+def proc(n):
     if n % 2 == 0:
         return n//2
     else:
         return (n * 3) + 1
 
-def proc(i, j):
+def getMaxCycle(i, j):
     cycles = []
     for n in range(i, j+1):
         loop = 0
@@ -17,13 +17,13 @@ def proc(i, j):
             if n > i and (n-i) < len(cycles):
                 loop += (cycles[n-i]-1)
                 break
-            n = func(n)
+            n = proc(n)
         cycles.append(loop)
-    msg = f'{i} {j} {max(cycles)}'
-    print(msg)
+    return max(cycles)
 
 start = time.time()
-proc(1, 10)
-proc(100, 200)
-proc(1000, 2000)
+input = [(1, 10), (100, 200), (1000, 2000)]
+for (i, j) in input:
+    msg = f'{i} {j} {getMaxCycle(i, j)}'
+    print(msg)
 print(time.time()-start)
