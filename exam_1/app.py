390c7d32f052168dcdb627f98a2a776d41ea89ea
wtchoi <wtchoi@osteosys.com>
2021-11-15 16:29:19 +0900

    recursive added

--- exam_1/app.py
+++ exam_1/app.py
@@ -35,6 +35,17 @@ def getMaxCycleWithTable(i, j):
 
     return max(cycles)
 
+def getCycle(n, loop):
+    if n == 1:
+        return loop
+    return getCycle(proc(n), loop+1)
+
+def getMaxCycleByRecursive(i, j):
+    cycles = []
+    for n in range(i, j+1):
+        cycles.append(getCycle(n, 1))
+    return max(cycles)
+
 input = [(1, 10), (100, 200), (1000, 2000)]
 print('without table:')
 start = time.time()
@@ -46,4 +57,10 @@ print('with table:')
 start = time.time()
 for (i, j) in input:
     print(f'{i} {j} {getMaxCycleWithTable(i, j)}')
+print(time.time()-start)
+
+print('by recursive:')
+start = time.time()
+for (i, j) in input:
+    print(f'{i} {j} {getMaxCycleByRecursive(i, j)}')
 print(time.time()-start)
